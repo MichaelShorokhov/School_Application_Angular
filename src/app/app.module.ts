9abf90c88a2587659da8d192de022036c20636a5
MichaelShorokhov <michael.shorokhov@gmail.com>
2018-11-14 10:55:22 +0200

    Localization finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,113 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
 import { AppComponent } from './app.component';
+import { MarkComponent } from './mark/mark.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { TermComponent } from './term/term.component';
+import { CourseComponent } from './course/course.component';
+import { GroupComponent } from './group/group.component';
+import { MarkForLessonComponent } from './mark-for-lesson/mark-for-lesson.component';
+import { SubjectComponent } from './subject/subject.component';
+import { LessonComponent } from './lesson/lesson.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
+import { NavigationComponent } from './navigation/navigation.component';
+import { TeacherGroupComponent } from './teacher-group/teacher-group.component';
+import { TeacherSubjectComponent } from './teacher-subject/teacher-subject.component';
+import { StudentSubjectComponent } from './student-subject/student-subject.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'mark',
+    component: MarkComponent
+  },
+  {
+    path: 'course',
+    component: CourseComponent
+  },
+  {
+    path: 'group',
+    component: GroupComponent
+  },
+  {
+    path: 'lesson',
+    component: LessonComponent
+  },
+  {
+    path: 'marksForLesson',
+    component: MarkForLessonComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'subject',
+    component: SubjectComponent
+  },
+  {
+    path: 'teacher',
+    component: TeacherComponent
+  },
+  {
+    path: 'term',
+    component: TermComponent
+  },
+  {
+    path: 'teacher_group',
+    component: TeacherGroupComponent
+  },
+  {
+    path: 'teacher_subject',
+    component: TeacherSubjectComponent
+  },
+  {
+    path: 'student_subject',
+    component: StudentSubjectComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkComponent,
+    StudentComponent,
+    TeacherComponent,
+    TermComponent,
+    CourseComponent,
+    GroupComponent,
+    MarkForLessonComponent,
+    SubjectComponent,
+    LessonComponent,
+    NavigationComponent,
+    TeacherGroupComponent,
+    TeacherSubjectComponent,
+    StudentSubjectComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
